815ebb50c09fd9d8e79ad25784f683204fe1290c
Brandon Powell <bpowell29a@gmail.com>
2012-12-24 01:14:39 -0500

    Updated installer for dircolors-8.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+#zshrc
 if [[ -a ~/.zshrc ]];
 then
 	echo "Backing up old ~/.zshrc"
@@ -8,7 +9,9 @@ fi
 
 echo "Creating symlink for ~/.zshrc"
 ln -s `pwd`/.zshrc ~/.zshrc
+#####
 
+#.shellutils
 if [[ -d ~/.shellutils ]];
 then
 	echo "Backing up old ~/.shellutils"
@@ -17,4 +20,14 @@ fi
 
 echo "Creating symlink for ~/.shellutils"
 ln -s `pwd`/.shellutils ~/.shellutils
+#####
 
+#dircolors
+if [[ -a ~/.dircolors-8 ]];
+then
+	echo "Backing up old ~/.dircolors-8"
+	mv ~/.dircolors-8 ~/.dircolors-8-backup
+fi
+
+echo "Creating symlink for ~/.dircolors-8"
+ln -s `pwd`/.dircolors-8 ~/.dircolors-8
