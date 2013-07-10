9335c5177cb8a56095504344768d950ab263cc37
Brandon Powell <bjpowel2@oakland.edu>
2013-07-10 16:41:23 -0400

    Added xinitrc.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-files=".zshrc .vimrc .vim .tmux.conf .shellutils"
+files=".zshrc .vimrc .vim .tmux.conf .shellutils .xinitrc"
 for file in $files; do
 	echo "Creating symlink for $file."
 	ln -s $file $HOME/$file
