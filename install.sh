26cdf491e2115fdefe970da6b8fe37f03ff5df33
Brandon Powell <bpowell29a@gmail.com>
2018-08-23 10:44:56 -0700

    tmux plugin manager

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 # Setup global gitignore
 cp .gitignore ~/
 git config --global core.excludesfile ~/.gitignore
+
+# tmux setup
+cp .tmux.conf ~/
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "In tmux press `prefix + I` to install plugins"
